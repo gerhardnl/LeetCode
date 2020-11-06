29786e560100533edbc166e51f42866ed9e3f096
Gerhard <gerhard.n.larsen@hotmail.com>
2020-11-06 03:26:47 +0100

    no comment

--- src/com/company/MaximumDepthofBinaryTree.java
+++ src/com/company/MaximumDepthofBinaryTree.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class MaximumDepthofBinaryTree {
     public int maxDepth(TreeNode5 root) {
-        if(root != null){
+        if (root != null) {
             int left = maxDepth(root.left);
             int right = maxDepth(root.right);
             return Math.max(left + 1, right + 1);
@@ -10,15 +10,22 @@ public class MaximumDepthofBinaryTree {
         return 0;
     }
 }
+
 class TreeNode5 {
-      int val;
-      TreeNode5 left;
-      TreeNode5 right;
-      TreeNode5() {}
-      TreeNode5(int val) { this.val = val; }
-      TreeNode5(int val, TreeNode5 left, TreeNode5 right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
+    int val;
+    TreeNode5 left;
+    TreeNode5 right;
+
+    TreeNode5() {
+    }
+
+    TreeNode5(int val) {
+        this.val = val;
+    }
+
+    TreeNode5(int val, TreeNode5 left, TreeNode5 right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
--- src/com/company/MaximumDepthofNAryTree.java
+++ src/com/company/MaximumDepthofNAryTree.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public class MaximumDepthofNAryTree {
     public int maxDepth(Node6 root) {
-        if(root != null){
+        if (root != null) {
             int depth = 0;
-            for(Node6 node : root.children){
+            for (Node6 node : root.children) {
                 depth = Math.max(depth, maxDepth(node));
             }
             return depth + 1;
@@ -19,7 +19,8 @@ class Node6 {
     public int val;
     public List<Node6> children;
 
-    public Node6() {}
+    public Node6() {
+    }
 
     public Node6(int _val) {
         val = _val;
